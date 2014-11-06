b9aa03b7f56c5b3b446b9d4d084febeb08a0f5c4
sanemat <o.gata.ken@gmail.com>
2014-11-06 17:11:40 +0900

    Update translation

--- js/i18n.js
+++ js/i18n.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
   i18n.init({ debug: true }, function() {
     // save to use translation function as resources are fetched
-    $("#header").i18n();
-    $("headline").i18n();
+    $('body').i18n();
   });
 });
